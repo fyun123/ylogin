ff9712c19dc706853578c5283835bf64bb841c35
fyun123 <1184384017@qq.com>
2021-03-20 15:00:13 +0800

    初始化项目结构

---
+++ ylogin-user/src/main/java/com/whut/ylogin/user/YloginUserApplication.java
@@ -0,0 +1,13 @@
+package com.whut.ylogin.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YloginUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YloginUserApplication.class, args);
+    }
+
+}
